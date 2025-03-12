e5e60b362ccc0422a8fefbc768db50f33fbec279
Albert <albertoma24@gmail.com>
2025-03-11 19:22:34 -0700

    Html and Css

---
+++ app.js
@@ -0,0 +1,69 @@
+const triviaData = [
+    // Science Questions
+    {
+        question: "Which artist holds the record for the most albums sold worldwide?",
+        options: ["Taylor Swift ", "Michael Jackson", "The Beatles", "Madonna"],
+        correctAnswer: "Michael Jackson"
+      },      
+      
+    {
+      question: "What is the largest organ in the human body?",
+      options: ["Brain", "Heart", "Skin", "Liver"],
+      correctAnswer: "Skin"
+    },
+  
+    // History Questions
+    {
+        question: "What was the name of the failed invasion that took place in Cuba during John F. Kennedy's presidency, intended to overthrow Fidel Castro?",
+        options: ["The Cuban Missile Crisis", "Bay of Pigs Invasion", "Operation Mongoose", "The Cuban Revolution"],
+        correctAnswer: "Bay of Pigs Invasion"
+          
+    },
+    {
+        question: "In what year did the Berlin Wall fall, symbolizing the end of the Cold War and the reunification of Germany?",
+        options: ["1987", "1989", "1991", "1993"],
+        correctAnswer: "1989"
+      },
+      
+      {
+        question: "Which notorious mafia boss was the leader of the Gambino crime family and was famously convicted for his role in numerous criminal activities, including murder?",
+        options: ["Al Capone", "John Gotti", "Lucky Luciano", "Vito Corleone"],
+        correctAnswer: "John Gotti"
+      },
+      
+  
+    // Geography Questions
+    {
+        question: "In which country is Machu Picchu, the ancient Incan city, located?",
+        options: ["Mexico", "Peru", "Chile", "Ecuador"],
+        correctAnswer: "Peru"
+      },
+      
+      {
+        question: "During which major historical event was the construction of the Empire State Building completed, symbolizing resilience and hope?",
+        options: ["World War I", "The Great Depression", "The Roaring Twenties", "The Civil Rights Movement"],
+        correctAnswer: "The Great Depression"
+      },
+  
+    // Pop Culture Questions
+    {
+        question: "Who wrote the famous 1925 novel *The Great Gatsby*, a classic of American literature?",
+        options: ["Ernest Hemingway", "F. Scott Fitzgerald", "John Steinbeck", "Mark Twain"],
+        correctAnswer: "F. Scott Fitzgerald"
+      },
+    
+     {
+        question: "Where was hip-hop music born in the 1970s, emerging as a cultural movement?",
+        options: ["Harlem", "The Bronx", "Brooklyn", "Compton"],
+        correctAnswer: "The Bronx"
+        },
+          
+    {
+        question: "Which sports team holds the record for the most championships in the history of professional sports?",
+        options: ["New York Yankees", "Los Angeles Lakers", "Real Madrid", "New England Patriots"],
+        correctAnswer: "New York Yankees"
+      },
+      
+  ];
+  
+  
